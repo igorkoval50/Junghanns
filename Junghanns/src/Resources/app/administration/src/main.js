7b06bf619c6f79387805d5536bd00a94f4734c6b
user <igor.k@ioksa.company>
2021-03-03 01:20:43 +0200

    style manufacture plugin

---
+++ Junghanns/src/Resources/app/administration/src/main.js
@@ -0,0 +1,7 @@
+import './module/sw-cms/blocks/text-image/image-brands';
+
+import deDE from './module/sw-cms/snippet/de-DE.json';
+import enGB from './module/sw-cms/snippet/en-GB.json';
+
+Shopware.Locale.extend('de-DE', deDE);
+Shopware.Locale.extend('en-GB', enGB);
---
+++ Junghanns/src/Resources/app/storefront/src/allco-swiper-slider/allco-swiper-slider.plugin.js
@@ -0,0 +1,72 @@
+import Plugin from 'src/plugin-system/plugin.class';
+import Swiper from '@swiper/bundle';
+
+export default class AllcoSwiperSlider extends Plugin {
+    init() {
+        console.log('Plugin is initialized');
+
+        //stick header
+        window.onscroll = function() {myFunction()};
+        var header = document.querySelector(".nav-main");
+        var sticky = header.offsetTop;
+        function myFunction() {
+            if (window.pageYOffset > sticky) {
+                header.classList.add("sticky");
+            } else {
+                header.classList.remove("sticky");
+            }
+        }
+
+        let isSwiperExist = document.querySelector('.swiper-container');
+        if (isSwiperExist === null)
+        {
+            console.log('null');
+            return;
+        }
+
+        if (document.querySelectorAll('.swiper-container-brands-element').length > 0) {
+            //Layout brands element
+
+            let swiperBrandsElement = new Swiper('.swiper-container-brands-element', {
+                slidesPerView: 6,
+                spaceBetween: 60,
+                freeMode: true,
+                loop: false,
+                loopFillGroupWithBlank: true,
+                watchSlidesVisibility: true,
+                navigation: {
+                    nextEl: '.swiper-button-next',
+                    prevEl: '.swiper-button-prev',
+                },
+                breakpoints: {
+                    0: {
+                        slidesPerView: 3,
+                        spaceBetween: 15,
+                    },
+                    576: {
+                        slidesPerView: 3,
+                        spaceBetween: 30,
+                    },
+                    992: {
+                        slidesPerView: 5,
+                        spaceBetween: 30,
+                    },
+                    1025: {
+                        slidesPerView: 6,
+                        spaceBetween: 60,
+                    },
+                }
+            });
+
+            // const swiperPrevTwo = document.getElementById('swiperPrevTwo');
+            // const swiperNextTwo = document.getElementById('swiperNextTwo');
+            //
+            // swiperPrevTwo.addEventListener('click', () => {
+            //     swiperBrandsElement.slidePrev();
+            // });
+            // swiperNextTwo.addEventListener('click', () => {
+            //     swiperBrandsElement.slideNext();
+            // });
+        }
+    }
+}
