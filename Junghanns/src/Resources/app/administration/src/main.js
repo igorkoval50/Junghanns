d160ddbd164e8e9e5aff8d421d88e904951e796e
user <igor.k@ioksa.company>
2021-03-08 15:25:20 +0200

    add four product element

--- Junghanns/src/Resources/app/administration/src/main.js
+++ Junghanns/src/Resources/app/administration/src/main.js
@@ -1,5 +1,6 @@
 import './module/sw-cms/blocks/text-image/image-brands';
 import './module/sw-cms/blocks/text-image/image-text-row';
+import './module/sw-cms/blocks/commerce/product-four-column';
 
 import deDE from './module/sw-cms/snippet/de-DE.json';
 import enGB from './module/sw-cms/snippet/en-GB.json';
