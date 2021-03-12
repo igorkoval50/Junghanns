bac2f6d8a49d6858805ba46061d3c135358f2e76
user <igor.k@ioksa.company>
2021-03-12 17:21:40 +0200

    create four text element

--- Junghanns/src/Resources/app/administration/src/main.js
+++ Junghanns/src/Resources/app/administration/src/main.js
@@ -1,7 +1,10 @@
 import './module/sw-cms/blocks/text-image/image-brands';
 import './module/sw-cms/blocks/text-image/image-text-row';
 import './module/sw-cms/blocks/text-image/image-text-row-two';
+
 import './module/sw-cms/blocks/commerce/product-four-column';
+
+import './module/sw-cms/blocks/text/text-four-column';
 
 import deDE from './module/sw-cms/snippet/de-DE.json';
 import enGB from './module/sw-cms/snippet/en-GB.json';
